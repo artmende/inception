9817bd7cacc1bc645e32be579bab3e9944b5a9ff
artmende <artmende@student.s19.be>
2022-11-15 16:41:44 +0100

    nginx container seems to work

--- srcs/requirements/nginx/tools/nginx_script.sh
+++ srcs/requirements/nginx/tools/nginx_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+rm -rf /etc/nginx/sites-enabled/default
+mv /tools/default.conf /etc/nginx/conf.d/
+
+
 nginx -g "daemon off;"
 
-echo "reached here" > /reached_here.txt
 
-rm -rf /etc/nginx/sites-available/default
-mv /tools/default.conf /etc/nginx/conf.d/
-nginx -s reload
