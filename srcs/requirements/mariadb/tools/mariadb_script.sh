9aa5544bf698b8953bd71c425e5429567e226404
artmende <artmende@student.s19.be>
2022-11-21 16:07:12 +0100

    rewriting the project. Did modifications on mariadb, now editing wordpress file

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -2,10 +2,11 @@
 
 service mysql start
 
-echo "CREATE DATABASE wordpress;" | mariadb -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mariadb -u root --skip-password
+echo "CREATE DATABASE $WORDPRESS_DB_NAME;" | mariadb -u root --skip-password
+echo "CREATE USER '$WORDPRESS_DB_USER'@'localhost' IDENTIFIED BY '$WORDPRESS_DB_USER_PASSWORD';" | mariadb -u root --skip-password
+echo "GRANT ALL ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'localhost';" | mariadb -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mariadb -u root --skip-password
-echo "update mysql.user set plugin='' where user='root';" | mariadb -u root --skip-password;
+# echo "update mysql.user set plugin='' where user='root';" | mariadb -u root --skip-password; what's that ?
 
 service mysql stop
 
