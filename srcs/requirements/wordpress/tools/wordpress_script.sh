1f985463a3b39d6799d6b4c0a4ff4202ebcd9a98
artmende <artmende@student.s19.be>
2022-11-22 17:46:10 +0100

    problem with wordpress connecting to mariadb

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd /www_folder && wget https://wordpress.org/latest.tar.gz
+cd /var/www/html && wget https://wordpress.org/latest.tar.gz
 
-cd /www_folder && tar -xf latest.tar.gz
+cd /var/www/html && tar -xf latest.tar.gz
 
-rm -f /www_folder/latest.tar.gz
+rm -f /var/www/html/latest.tar.gz
 
-mv /tools/wp-config.php /www_folder/wordpress
+mv /tools/wp-config.php /var/www/html/wordpress
+
+rm -f /etc/php/7.3/fpm/pool.d/www.conf
+mv /tools/www.conf /etc/php/7.3/fpm/pool.d/
 
 php-fpm7.3 --nodaemonize
