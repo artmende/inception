c48444e997cf37ae4eb7800ecd3b4db0f5373d0a
artmende <artmende@student.s19.be>
2022-11-24 18:17:56 +0100

    wordpress and mariadb finally work

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# chown -R mysql:mysql /var/lib/mysql
+# mkdir -p /var/run/mysqld
+# mkfifo /var/run/mysqld/mysqld.sock
+# touch /var/run/mysqld/mysqlf.pid
+# chown -R mysql:mysql /var/run/mysqld
+
+# rm -f /etc/mysql/mariadb.cnf
+mv /tools/mariadb.cnf /etc/mysql/
+
+echo "before start"
+
 service mysql start
 
 echo "before create database with name $WORDPRESS_DB_NAME" && echo "CREATE DATABASE $WORDPRESS_DB_NAME;" | mariadb
-echo "before create user" && echo "CREATE USER '$WORDPRESS_DB_USER'@'localhost' IDENTIFIED BY '$WORDPRESS_DB_USER_PASSWORD';" | mariadb
-echo "before grant" && echo "GRANT ALL ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'localhost';" | mariadb
+echo "before create user" && echo "CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_USER_PASSWORD';" | mariadb
+echo "before grant" && echo "GRANT ALL ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';" | mariadb
 # echo "UPDATE mysql.user SET Password=PASSWORD('YOURNEWPASSWORDD') WHERE User='root';" | mariadb -u root --skip-password
 echo "before flush" && echo "FLUSH PRIVILEGES;" | mariadb
 echo "after flush"
@@ -12,13 +23,9 @@ echo "after flush"
 # echo "update mysql.user set plugin='' where user='root';" | mariadb -u root --skip-password; what's that ?
 
 echo "before all those commands"
-rm -f /etc/mysql/mariadb.cnf
-mv /tools/mariadb.cnf /etc/mysql/
 
-chown -R mysql:mysql /var/lib/mysql
-mkdir -p /var/run/mysqld
-touch /var/run/mysqld/mysqlf.pid
-chown -R mysql:mysql /var/run/mysqld
+
+
 
 echo "after all those commands"
 
